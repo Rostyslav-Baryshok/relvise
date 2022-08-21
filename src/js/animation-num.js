186977bf174b5c5d2170a6a767a5d23360a2f79b
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-08-21 21:21:08 +0300

    add animation number

---
+++ src/js/animation-num.js
@@ -0,0 +1,28 @@
+let time = 2,
+  cc = 1;
+$(window).scroll(function () {
+  $('#counter').each(function () {
+    let cPos = $(this).offset().top,
+      topWindow = $(window).scrollTop();
+    if (cPos < topWindow + 400) {
+      if (cc < 2) {
+        $('.number').addClass('viz');
+        $('div').each(function () {
+          let i = 1,
+            num = $(this).data('num'),
+            step = (1000 * time) / num,
+            that = $(this),
+            int = setInterval(function () {
+              if (i <= num) {
+                that.html(i);
+              } else {
+                cc = cc + 1;
+                clearInterval(int);
+              }
+              i++;
+            }, step);
+        });
+      }
+    }
+  });
+});
