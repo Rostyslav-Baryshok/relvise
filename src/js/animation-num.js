156e045147d6e71af0d57c922795e139a3555409
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-08-22 23:10:27 +0300

    update section contacts

--- src/js/animation-num.js
+++ src/js/animation-num.js
@@ -4,7 +4,7 @@ $(window).scroll(function () {
   $('#counter').each(function () {
     let cPos = $(this).offset().top,
       topWindow = $(window).scrollTop();
-    if (cPos < topWindow + 550) {
+    if (cPos < topWindow + 500) {
       if (cc < 2) {
         $('.number').addClass('viz');
         $('div').each(function () {
