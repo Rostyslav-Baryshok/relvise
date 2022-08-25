5609c83c7e7b8dc4498ab586fa5a060cb897fabd
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-08-25 16:31:06 +0300

    update animation

--- src/js/animation-num.js
+++ src/js/animation-num.js
@@ -1,28 +1,64 @@
-let time = 2,
-  cc = 1;
-$(window).scroll(function () {
-  $('#counter').each(function () {
-    let cPos = $(this).offset().top,
-      topWindow = $(window).scrollTop();
-    if (cPos < topWindow + 500) {
-      if (cc < 2) {
-        $('.number').addClass('viz');
-        $('div').each(function () {
-          let i = 1,
-            num = $(this).data('num'),
-            step = (1000 * time) / num,
-            that = $(this),
-            int = setInterval(function () {
-              if (i <= num) {
-                that.html(i);
-              } else {
-                cc = cc + 1;
-                clearInterval(int);
-              }
-              i++;
-            }, step);
-        });
-      }
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+  // Функція ініціалізації
+  function digitsCountersInit(digitsCountersItems) {
+    let digitsCounters = digitsCountersItems
+      ? digitsCountersItems
+      : document.querySelectorAll('[data-digits-counter]');
+    if (digitsCounters.length) {
+      digitsCounters.forEach(digitsCounter => {
+        digitsCountersAnimate(digitsCounter);
+      });
     }
-  });
-});
+  }
+  // Функція анімації
+  function digitsCountersAnimate(digitsCounter) {
+    let startTimestamp = null;
+    const duration = parseInt(digitsCounter.dataset.digitsCounter)
+      ? parseInt(digitsCounter.dataset.digitsCounter)
+      : 1500;
+    const startValue = parseInt(digitsCounter.innerHTML);
+    const startPosition = 0;
+    const step = timestamp => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      digitsCounter.innerHTML = Math.floor(
+        progress * (startPosition + startValue)
+      );
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  // Пуск при завантаженні сторінки
+  // digitsCountersInit();
+
+  // Пуск при скроллі (появі блока з лічільниками)
+  let options = {
+    threshold: 0.5,
+  };
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const targetElement = entry.target;
+        const digitsCountersItems = targetElement.querySelectorAll(
+          '[data-digits-counter]'
+        );
+        if (digitsCountersItems.length) {
+          digitsCountersInit(digitsCountersItems);
+        }
+        // Вимкнути відслідковування після спрацювання
+        observer.unobserve(targetElement);
+      }
+    });
+  }, options);
+
+  let sections = document.querySelectorAll('.advantages__body');
+  if (sections.length) {
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  }
+}
