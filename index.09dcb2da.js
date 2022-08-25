d7ffbecc578ca3ba96f95ae6302ded4f423f549e
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-08-25 14:18:06 +0000

    Deploying to gh-pages from @ Rostyslav-Baryshok/relvise@a0ba6f5f04b0edbeea86f70173efc4462b816d19 🚀

---
+++ index.09dcb2da.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){let e=document.querySelector(".header");window.pageYOffset>2&&e.classList.add("header_fixed"),e.classList.remove("header_fixed")};
+//# sourceMappingURL=index.09dcb2da.js.map
---
+++ index.81402525.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){var e=document.querySelector(".header");window.pageYOffset>2&&e.classList.add("header_fixed"),e.classList.remove("header_fixed")};
+//# sourceMappingURL=index.81402525.js.map
